509516d87569de99392a381c2b1e0b4168c6a750
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-05-26 18:31:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace POLYMORPHISM
+{
+    // Create an interface called IQuittable
+    interface IQuittable
+    {
+        // Define a void method called Quit()
+        void Quit();
+    }
+
+    // Create a class called Employee and inherit from the IQuittable interface
+    class Employee : IQuittable
+    {
+        // Implement the Quit() method from the interface
+        public void Quit()
+        {
+            Console.WriteLine("Employee quits the job.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use polymorphism to create an object of type IQuittable and instantiate it with an Employee object
+            IQuittable quittable = new Employee();
+
+            // Call the Quit() method on the object, which invokes the Quit() method implemented in the Employee class
+            quittable.Quit();
+
+            // Wait for user input before closing the console window
+            Console.ReadLine(); 
+        }
+    }
+    
+}
